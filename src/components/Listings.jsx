42d45d42447fa840724cafef2520ca8f8a99d848
Shad <shadrais321@outlook.com>
2022-10-18 18:46:49 +0530

    Styling Fixed

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -5,10 +5,14 @@ import useListing from '../context/listingContext'
 const Listings = () => {
   const { listing } = useListing()
   return (
-    <div className='listings'>
-      {listing.map((data) => {
-        return <Card key={data.id} data={data} />
-      })}
+    <div className={`${listing.length && 'listings'}`}>
+      {listing.length > 0 ? (
+        listing.map((data) => {
+          return <Card key={data.id} data={data} />
+        })
+      ) : (
+        <h1 className='fav'>No Listings</h1>
+      )}
     </div>
   )
 }
